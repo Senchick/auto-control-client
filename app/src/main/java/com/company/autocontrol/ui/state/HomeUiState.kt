05019114d808daa8c2c282f043707fb305c26b9a
Senchick <senchickpro@gmail.com>
2023-06-27 09:24:24 +0300

    feat: add booking logic

--- app/src/main/java/com/company/autocontrol/ui/state/HomeUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.company.autocontrol.ui.state
-
-import com.company.autocontrol.data.model.booking.RoadSection
-import java.time.LocalDate
-
-data class HomeUiState(
-    val selectedRoadSection: RoadSection?,
-    val selectedDate: LocalDate?
-)
